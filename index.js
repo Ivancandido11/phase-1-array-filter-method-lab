b0473c5088536a7e286067406fa57a76b9412fce
Ivan Candido <ivan.candido@powerhrg.com>
2021-05-26 09:04:40 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, name){
+    return (array.filter(function(cb){return cb === name || cb === name.toLowerCase()}))
+}
+
+function fuzzyMatch(array, letters){
+    return (array.filter(function(cb){return cb.startsWith(letters, 0)}))
+}
+
+function matchName(obj, nameElement){
+    return (obj.filter(function(cb){return cb === nameElement}))
+}
+
+function matchName(obj, nameElement){
+    return obj.filter(element => element.name === nameElement)
+}
